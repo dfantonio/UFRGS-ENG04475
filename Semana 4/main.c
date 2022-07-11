a3bc541e0873a86b958b81a38b6505eb955661c4
Antônio Della Flora <antonio@truepay.com.br>
2022-07-11 14:03:04 -0300

    código da semana 4

--- Semana 4/main.c
+++ Semana 4/main.c
@@ -1,36 +1,82 @@
-#include <reg51.h>
+#include <reg52.h>
 
-sbit pino0 = P1 ^ 7;
+#define BOUNCE 7
 
-void main(void) {
-  char oldStatus = 0, count = 0, debouncedInput = 0;
 
-  TMOD = 0x10; // habilita o timer 1 para contador de 16 bits
-  TR1 = 1;     // ativa o timer 1
+sbit pinSaida = P3^4; //Define o pino de saída
 
-  // TL e TH definem a contagem de 16 bits. Nesse caso vão contar por 10 ms.
-  TH1 = 0xb8;
-  TL1 = 0x62;
-  TF1 = 0;
-
+// Função para fazer o debouce.
+// Os valores de linha e coluna variam entre 0 e 3.
+unsigned char debounce (char linha, char coluna) {
+	char oldStatus = 0, count = 0, pinoColuna;
+	
+	pinoColuna = P0 & (1 << coluna); // Define o pino da coluna desejada;
+	
+	
+  TMOD = 0x01; // habilita o timer 0 para contador de 16 bits
+  // TL e TH definem a contagem de 16 bits. Nesse caso vão contar por 1ms.
+  TH0 = 0xf8;
+  TL0 = 0xcd;
+  TF0 = 0;
+  TR0 = 1;     // ativa o timer 0
+	
+	P0 = ~(1 << (7 - linha)); // Define todos os valores de P0 como 1 exceto a linha desejada.
+	
   while (1) {
-    if (TF1) { // Quando o timer1 estourar
+    if (TF0) { // Quando o timer0 estourar
 
       // Reseta o timer para contar novamente
-      TH1 = 0xb8;
-      TL1 = 0x62;
-      TF1 = 0;
+      TH0 = 0xb8;
+      TL0 = 0x00;
+      TF0 = 0;
 
       // se o pino tiver o mesmo valor da última leitura
-      if (pino0 == oldStatus) count++;
+      if (pinoColuna == oldStatus) count++;
       else {
-        oldStatus = pino0; // Atualiza o status do pino
+        oldStatus = pinoColuna; // Atualiza o status do pino
         count = 0;         // zera o contador
       }
 
-      if (count >= 10) { // Caso o contador seja maior ou igual a 10 podemos confiar na entrada
-        debouncedInput = pino0;
+      if (count >= BOUNCE) { // Caso o contador seja maior ou igual ao valor de bounce podemos confiar na entrada
+        return !pinoColuna; // Retorna o valor negado para que o valor seja 1 quando o botão estiver pressionado. (fica mais intuitivo)
       }
     }
   };
+}
+
+
+//Função de interrupção do timer 2
+void mudaSaida(void) interrupt 5 {
+		TF2 = 0; // Zera a flag de estouro
+		pinSaida = !pinSaida; // Inverte a saída
+}
+
+void main(void) {
+  char col1, col2;
+	
+	TF2=0; // Zera a flag de estouro
+	TR2=1; // Habilita a contagem
+	
+	TH2 = 0xDC; // Valor da parte alta para fazer uma contagem de 5 ms
+	TL1 = 0x00; // Valor da parte baixa para fazer uma contagem de 5 ms
+	RCAP2H = 0xDC; // Valor da parte alta para fazer uma contagem de 5 ms após o estouro
+	RCAP2L = 0x00; // Valor da parte baixa para fazer uma contagem de 5 ms após o estouro
+	
+	EA=1; // Permite ativar todas as interrupções
+	ET2=1; // Habilita a interrupção do timer 2
+		
+	while(1){
+		col1 = debounce(2,0); // Lê o valor do pino na 3 linha e 1 coluna
+		col2 = debounce(2,1); // Lê o valor do pino na 3 linha e 2 coluna		
+		
+		if(col1) { // Define o timer 2 para a frequência de 100 HZ
+			RCAP2H = 0xDC;
+			RCAP2L = 0x00;
+		}
+		
+		if(col2) { // Define o timer 2 para a frequência de 200 HZ
+			RCAP2H = 0xEE;
+			RCAP2L = 0x00;
+		}
+	}
 }
