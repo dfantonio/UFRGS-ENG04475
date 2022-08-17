7ac8496d10d32804ef319c67ddd82ee1e5d971a3
vtverona2 <vtverona2@gmail.com>
2022-08-16 21:05:50 -0300

    Adição da navegação do menu

--- projeto-urna/urna/lib/estados/estados.h
+++ projeto-urna/urna/lib/estados/estados.h
@@ -2,3 +2,9 @@
 
 void autentica(struct Urna *urna);
 void menu(struct Urna *urna);
+void exibeHora(struct Urna *urna);
+void defineHora(struct Urna *urna);
+void trocaEstadoUrna(struct Urna *urna);
+void auditoria(struct Urna *urna);
+void validaEleitor(struct Urna *urna);
+void geraRelatorio(struct Urna *urna);
